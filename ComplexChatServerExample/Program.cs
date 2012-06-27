eb00b738679e949e52d174f8f2f49aa3fc3e008e
elpablos@gmail.com <elpablos@gmail.com@6c6ce49b-89d1-cebb-24b1-91578a92f2c7>
2012-06-27 14:35:29 +0000

    - reading argument from command line (server)

--- ComplexChatServerExample/Program.cs
+++ ComplexChatServerExample/Program.cs
@@ -19,29 +19,42 @@ namespace ComplexWpfChatServerExample
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            // inicializace promennych
-            string server = string.Empty;
-            int port = 0;
-            // pokud jsou predany parametrem, tak je nacteme
-            if (args.Length > 1)
+            try
             {
-                server = args[0];
-                port =  Int32.Parse(args[1]);
+                Console.WriteLine("Startuju chat server..");
+                // inicializace promennych
+                string server = string.Empty;
+                int port = 0;
+                // pokud jsou predany parametrem, tak je nacteme
+                if (args.Length > 1)
+                {
+                    Console.WriteLine("Nalezeny argumenty..");
+                    server = args[0];
+                    port = Int32.Parse(args[1]);
+                }
+                // jinak vyuzijeme config
+                else
+                {
+                    Console.WriteLine("Nastaveni nacitam z configu..");
+                    server = AppSettingsReader.ReadProperty("ServerAddress");
+                    port = AppSettingsReader.ReadIntProperty("ServerPort");
+                }
+
+                Console.WriteLine("Ziskavam informace z DNS [{0}:{1}]..", server, port);
+                // zistani IPAddress
+                var he = Dns.GetHostEntry(server);
+                IPAddress ip = he.AddressList.FirstOrDefault(a => AddressFamily.InterNetwork == a.AddressFamily) ?? he.AddressList[0];
+
+                Console.WriteLine("Pokus o naslouchani na [{0}:{1}]..", server, port);
+                // odstartovani serveru
+                Server srv = new Server(ip, port);
+                srv.StartListen();
+                Console.WriteLine("Nasloucham..");
             }
-            // jinak vyuzijeme config
-            else
+            catch (Exception ex)
             {
-                server = AppSettingsReader.ReadProperty("ServerAddress");
-                port = AppSettingsReader.ReadIntProperty("ServerPort");
+                Console.Error.WriteLine(ex);
             }
-
-            // zistani IPAddress
-            var he = Dns.GetHostEntry(server);
-            IPAddress ip = he.AddressList.FirstOrDefault(a => AddressFamily.InterNetwork == a.AddressFamily) ?? he.AddressList[0];
-
-            // odstartovani serveru
-            Server srv = new Server(ip, port);
-            srv.StartListen();
         }
     }
 }
